fe9b8630828a88675254570e5d2d4f50f5daa4bb
Tayyab <mtayyab4846@gmail.com>
2025-10-01 09:39:31 +0500

    update

--- frontend/src/components/home/FeaturedProducts.jsx
+++ frontend/src/components/home/FeaturedProducts.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import "./style.css"
 
 const FeaturedProducts = () => {
+
+  const api_url = "https://67ff575158f18d7209f0cc07.mockapi.io/gamingstore/products";
+
   return (
     <div>
         <h1 className='fp-container'>Featured Products</h1>

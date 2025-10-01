1b3c50a12f53045b390d4507085ccee1a5466f2f
Tayyab <mtayyab4846@gmail.com>
2025-10-01 16:09:52 +0500

    update

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Hero from '../components/home/Hero';
 import FeaturedProducts from '../components/home/FeaturedProducts';
+import ProductCategory from '../components/home/ProductCategory';
 
 const Home = () => {
   return (
     <div>
         <Hero/>
         <FeaturedProducts/>
+        <ProductCategory/>
     </div>
   )
 }

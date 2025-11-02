fdfe5f5217f6bb39089b5f013db6aec4be74e49c
Tayyab <mtayyab3897@gmail.com>
2025-11-02 20:04:37 +0500

    updated-env

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL:
-    import.meta.env.VITE_API_URL ||
-    "https://tayyab-fyp-backend.onrender.com/api",
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
 });
 
 export default API;

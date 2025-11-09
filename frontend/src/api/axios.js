44f7c647c3fb352396c1b526eee8ce85229616f1
Tayyab <mtayyab3897@gmail.com>
2025-11-09 19:37:19 +0500

    update

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: `${import.meta.env.VITE_API_URL}/api`,
+  baseURL: import.meta.env.DEV
+    ? `${import.meta.env.VITE_API_URL_LOCAL}/api`
+    : `${import.meta.env.VITE_API_URL_PROD}/api`,
 });
 
 export default API;

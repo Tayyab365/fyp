b640d3ab9b8277de071508b14dc58a619238b3bb
Tayyab <mtayyab3897@gmail.com>
2025-11-23 18:01:01 +0500

    update

--- backend/server.js
+++ backend/server.js
@@ -37,4 +37,8 @@ app.get("/", (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => {
+  console.log(
+    `Server running on port ${PORT} (process.env.PORT: ${process.env.PORT})`
+  );
+});
